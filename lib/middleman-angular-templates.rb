f2c440c710756a1dbb410e9c1bbfa028bc4d37ba
Jeff J Smith <jffreyjs@gmail.com>
2014-05-01 15:10:48 -0400

    just use template_path

--- lib/middleman-angular-templates.rb
+++ lib/middleman-angular-templates.rb
@@ -14,7 +14,7 @@ class AngularTemplates < ::Middleman::Extension
           template_name = File.basename(filename, '.*')
           #next unless template_name.slice!(0) == '_'
           template_path = Pathname.new(File.join(File.dirname(filename), template_name)).relative_path_from(base_dir)
-          html << content_tag(:script, partial(template_path), type: "text/ng-template", id: "#{template_path}.html")
+          html << content_tag(:script, template_path, type: "text/ng-template", id: "#{template_path}.html")
         end
       end
     end
